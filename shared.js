bdc2f8b11d75726bb9fc0cb560822dc41197fa15
משפחת חדד <haim.hddd@gmail.com>
2020-10-29 13:55:30 +0200

    adding modal+menu-mobile

---
+++ shared.js
@@ -0,0 +1,42 @@
+var backdrop = document.querySelector('.backdrop');
+var modal = document.querySelector('.modal');
+var selectPlanButtons = document.querySelectorAll('.plan button');
+var modalNoButton = document.querySelector('.modal__action--negative');
+var toggleButton = document.querySelector('.toggle-button');
+var mobileNav = document.querySelector('.mobile-nav');
+
+
+console.dir(selectPlanButtons);
+
+selectPlanButtons.forEach(element =>{
+
+    element.addEventListener('click',(event)=>{
+        /* backdrop.style.display ='block';
+        modal.style.display = 'block'; */
+        backdrop.classList.add('open');
+        modal.classList.add('open');
+    });
+
+});
+
+[backdrop,modalNoButton].forEach(element=>{
+    element.addEventListener('click',()=>{
+        /* mobileNav.style.display='none'; */
+        mobileNav.classList.remove('open');
+        closeModal();
+    });
+});
+
+function closeModal(){
+    /* backdrop.style.display ='none';
+    modal.style.display = 'none'; */
+    backdrop.classList.remove('open');
+    modal.classList.remove('open');
+}
+
+toggleButton.addEventListener('click',()=>{
+    mobileNav.classList.add('open');
+    backdrop.classList.add('open');
+   /*  mobileNav.style.display='block';
+    backdrop.style.display='block'; */
+})
